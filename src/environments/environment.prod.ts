92052df17d0979894488866eb57b9e048d005be8
rusty-banks <anil.kumar2005.2100@gmail.com>
2019-12-16 04:57:36 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDCgqgvMTNGY8WauASctqF2LFBpmeRmFr8',
-    authDomain: 'oshop-a3ac3.firebaseapp.com',
-    databaseURL: 'https://oshop-a3ac3.firebaseio.com',
-    projectId: 'oshop-a3ac3',
-    storageBucket: 'oshop-a3ac3.appspot.com',
-    messagingSenderId: '244763184917',
-    appId: '1:244763184917:web:361a3c20125aa076e2e31c'
+    // Put your FB config properties here
   }
 };
